1dfd3a152616e648c92621fc29f06a1af7c5898b
Serendipity <plsun1108@163.com>
2021-05-30 22:29:03 +0800

    init-project 2021-05-30

---
+++ src/main/java/com/spl/serendipity928/Serendipity928Application.java
@@ -0,0 +1,13 @@
+package com.spl.serendipity928;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serendipity928Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Serendipity928Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spl/serendipity928/Serendipity928ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spl.serendipity928;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serendipity928ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
