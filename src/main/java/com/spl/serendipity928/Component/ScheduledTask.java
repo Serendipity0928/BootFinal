34b4348fe94df652de6ea1229d4593637b37d145
spl <plsun1108@163.com>
2021-06-16 00:27:20 +0800

    20210616 个人打卡完成

--- src/main/java/com/spl/serendipity928/Component/ScheduledTask.java
+++ src/main/java/com/spl/serendipity928/Component/ScheduledTask.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import sun.dc.pr.PRError;
 
+import javax.annotation.Resource;
 import java.time.LocalDateTime;
 
 @Component
@@ -18,6 +19,9 @@ public class ScheduledTask {
             "&city=北京市&province=北京市&address=北京市朝阳区东湖街道恒基伟业大厦BⅡ望京恒电大厦" +
             "&geo_api_info={\"type\":\"complete\",\"info\":\"SUCCESS\",\"status\":1,\"cEa\":\"jsonp_295677_\",\"position\":{\"Q\":40.00855,\"R\":116.48752000000002,\"lng\":116.48752,\"lat\":40.00855},\"message\":\"Get ipLocation success.Get address success.\",\"location_type\":\"ip\",\"accuracy\":null,\"isConverted\":true,\"addressComponent\":{\"citycode\":\"010\",\"adcode\":\"110105\",\"businessAreas\":[{\"name\":\"望京\",\"id\":\"110105\",\"location\":{\"Q\":39.996171,\"R\":116.47029299999997,\"lng\":116.470293,\"lat\":39.996171}},{\"name\":\"来广营\",\"id\":\"110105\",\"location\":{\"Q\":40.034234,\"R\":116.453779,\"lng\":116.453779,\"lat\":40.034234}}],\"neighborhoodType\":\"\",\"neighborhood\":\"\",\"building\":\"\",\"buildingType\":\"\",\"street\":\"望京东路\",\"streetNumber\":\"4号院\",\"country\":\"中国\",\"province\":\"北京市\",\"city\":\"\",\"district\":\"朝阳区\",\"township\":\"东湖街道\"},\"formattedAddress\":\"北京市朝阳区东湖街道恒基伟业大厦BⅡ望京恒电大厦\",\"roads\":[],\"crosses\":[],\"pois\":[]}" +
             "&gwdz=&is_move=0&move_reason=&move_remark=&realname=孙培林&number=SY1906412&uid=376685&created=1623661268&date=20210614&created_uid=300068&id=2994284&gwszdd=";
+
+    @Resource
+    private EmailService emailService;
 
     // 每天下午 17:01 执行
     @Scheduled(cron = "0 01 17 ? * * ")
@@ -31,8 +35,11 @@ public class ScheduledTask {
         if(reportMessage == null || (reportMessage.getE() != 0 && reportMessage.getE() != 1)) {
             // 打卡失败, 发送邮箱  https://segmentfault.com/a/1190000021587834
             System.out.println("scheduledTask is failed!, result="+result);
-
+            emailService.sendMail("878478652@qq.com", "自动打卡失败邮件通知",
+                    "系统自动打开失败，失败原因已反馈，请立即手动打卡; 接口反馈信息：" + result);
+            return;
         }
+
         System.out.println(result);
 
     }
--- src/main/java/com/spl/serendipity928/HelloController.java
+++ src/main/java/com/spl/serendipity928/HelloController.java
@@ -1,14 +1,21 @@
 package com.spl.serendipity928;
 
+import com.spl.serendipity928.Component.EmailService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class HelloController {
 
-  @RequestMapping("/school/report")
-  public String hello() {
+    @Resource
+    private EmailService emailService;
+
+    @RequestMapping("/school/report")
+    public String hello() {
+      emailService.sendMail("878478652@qq.com", "主题", "内容");
       return "Hello World";
-  }
+    }
   
 }
